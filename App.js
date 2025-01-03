b57d9b5fdf02149c337b647d69ab73cc6750440e
unknown <kapkotikartik69@gmail.com>
2025-01-03 19:21:12 +0530

    new div

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export const App = () => {
+  return (
+    <>
+    <div>Apphhghghgh</div>
+    <div>Apphhghghgh</div>
+    <div>Apphhghghgh</div>
+    <div>Apphhghghgh</div>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
